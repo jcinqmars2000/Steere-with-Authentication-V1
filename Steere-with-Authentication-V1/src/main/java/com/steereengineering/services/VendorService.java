f73843e2d4a64b3ab9efa0c02a5194733609b922
John Cinq-Mars <jcinqmars2000@yahoo.com>
2018-12-28 10:32:53 -0500

    Initial upload

---
+++ Steere-with-Authentication-V1/src/main/java/com/steereengineering/services/VendorService.java
@@ -0,0 +1,10 @@
+package com.steereengineering.services;
+
+import java.util.Set;
+
+import com.steereengineering.model.Vendor;
+
+public interface VendorService {
+	Set<Vendor> getVendorList();
+
+}
